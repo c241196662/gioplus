1bd306cd7e59a83c2a370bf7508de6e93563f1ca
c241196662 <120004589@qq.com>
2019-04-29 09:19:54 +0800

    android

--- src/android/Gioplus.java
+++ src/android/Gioplus.java
@@ -1,4 +1,6 @@
-package cordova-plugin-bakaan-gioplus;
+package cordova.plugin.bakaan.gioplus;
+
+import com.growingio.android.sdk.collection.GrowingIO;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -10,21 +12,52 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class gioplus extends CordovaPlugin {
+public class Gioplus extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        if (action.equals("getDeviceId")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.getDeviceId(message, callbackContext);
+            return true;
+        } else if (action.equals("getSessionId")) {
+            String message = args.getString(0);
+            this.getDeviceId(message, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    private void getDeviceId(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+
+            GrowingIO gio = GrowingIO.getInstance();
+            String deviceid = gio.getDeviceId();
+            JSONObject json = new JSONObject();
+            try {
+                json.put("deviceid", deviceid);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            callbackContext.success(json);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void getSessionId(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
+            GrowingIO gio = GrowingIO.getInstance();
+            String sessionid = gio.getSessionId();
+            JSONObject json = new JSONObject();
+            try {
+                json.put("sessionid", sessionid);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            callbackContext.success(json);
+
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
